bc5a1cee5046ce4e57f54ba50c94310c8e877681
betalk88 <79296473+betalk88@users.noreply.github.com>
2021-03-08 12:18:31 +0800

    Update firebase-messaging-sw.js

--- messaging/firebase-messaging-sw.js
+++ messaging/firebase-messaging-sw.js
@@ -1,7 +1,7 @@
 // Import and configure the Firebase SDK
 // These scripts are made available when the app is served or deployed on Firebase Hosting
 // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('https://www.gstatic.com/firebasejs/8.2.10/firebase-app.jss');
+importScripts('https://www.gstatic.com/firebasejs/8.2.10/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.2.10/firebase-messaging.js');
 
   var firebaseConfig = {
